d0f21ed9bb5b4c7eb5fe1a737f1839a0539ff163
Odskee <20175889+odskee@users.noreply.github.com>
2023-04-08 17:06:50 +0100

    Initial Commit
    Full Console App

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Ionic.Zlib;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Text;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var utf8 = new UTF8Encoding();
+
+            using (var client = new SubscriberSocket())
+            {
+                client.Options.ReceiveHighWatermark = 1000;
+                client.Connect("tcp://eddn.edcd.io:9500");
+                client.SubscribeToAnyTopic();
+                while (true)
+                {
+                    var bytes = client.ReceiveFrameBytes();
+                    var uncompressed = ZlibStream.UncompressBuffer(bytes);
+
+                    var result = utf8.GetString(uncompressed);
+
+                    Console.WriteLine(result + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
